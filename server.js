06c8eb4687e72f99cf2d618db6f4c4279f892285
tonga <grojas89@hotmail.com>
2022-06-24 00:12:32 -0300

    -desafio clase 26

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
 import express from 'express'
 import session from 'express-session'
-import passport from 'passport'
-import { Strategy } from 'passport-local'
+import MongoStore from 'connect-mongo';
+
 
 import mongoDb from './mongoDb.js'
+import { passportMiddleware, passportSessionHandler } from './src/middlewares/passport.js'
+import { authRouter } from './src/routers/authRouter.js'
+import { usersRouter } from './src/routers/usersRouter.js'
+
 
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(
-    session({
-      store: MongoStore.create({ mongoUrl: mongoDb.sessions }),
-      secret: 'secret',
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 500000,
-      },
-    })
-  );
+  session({
+    store: MongoStore.create({ mongoUrl: mongoDb.sessions }),
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 500000,
+    },
+  })
+);
+app.use(passportMiddleware)
+app.use(passportSessionHandler)
+
+
+
+app.use('/api/usuarios', usersRouter)
+app.use('/auth', authRouter)
+
+
+// listen
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`escuchando en puerto ${server.address().port}`)
+})
+
+
 
 
 
--- src/persistencia/users.js
+++ src/persistencia/users.js
@@ -1,14 +1,38 @@
 import mongoose from 'mongoose';
 import mongoDb from '../../mongoDb.js'
 
-await mongoose.connect(mongoDbConnectionString.users);
+await mongoose.connect(mongoDb.users);
 
 
 const db = mongoose.model('users', {
     id: Number,
-    nombre: String,
-    descripcion: String,
-    foto: String,
-    precio: Number,
-    stock: Number,
+    username: String,
+    password: String
   });
+
+
+export const getAll = async () => {
+  const data = await db.find({}, { _id: 0, __v: 0 });
+  return data
+}
+
+export const saveUser = async (user) => {
+  await db.create(user);
+}
+
+export const getById= async (i) => {
+  const user = await db.findOne({ id: i }, { _id: 0, __v: 0 });
+  if (!user) throw new Error('no existe un usuario con ese id')
+  return user
+}
+
+export const getByName = async (userName) => {
+  const user = await db.findOne({username: userName}, { _id: 0, __v: 0 })
+  if(!user) throw new Error('no existe un usuario con ese nombre')
+  return user
+}
+
+export const checkUsernameAvailability = async (userName) => {
+  const user = await db.findOne({username: userName}, { _id: 0, __v: 0 })
+  if (user) throw new Error('el nombre de usuario no está disponible')
+}
